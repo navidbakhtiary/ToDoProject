4169c29599c1d3dd259a150ee4625a5efd043bbe
navidbakhtiary <navidbakhtiary@yahoo.com>
2022-05-05 22:47:56 +0430

    Test: A test function was created to check that the user could not create a new label whose name has already been saved.

--- packages/navidbakhtiary/todo/tests/Feature/LabelTest.php
+++ packages/navidbakhtiary/todo/tests/Feature/LabelTest.php
@@ -13,6 +13,7 @@ class LabelTest extends TestCase
     use RefreshDatabase;
 
     private $api_prefix = '/todo/labels/';
+    private $api_add = '/todo/labels/add';
     private $bearer_prefix = 'Bearer ';
 
     public function testCreateLabelByAuthenticatedUser()
@@ -20,7 +21,8 @@ class LabelTest extends TestCase
         $user = factory(User::class)->create();
         $token = $user->createToken('test-token');
         $label = factory(Label::class)->make();
-        $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token->plainTextToken])->postJson($this->api_prefix . 'add', ['name' => $label->name]);
+        $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token->plainTextToken])->
+            postJson($this->api_add, ['name' => $label->name]);
         $response->assertCreated()->assertJsonFragment(['name' => $label->name]);
         $this->assertDatabaseHas('labels', ['name' => $label->name]);
     }
@@ -30,7 +32,7 @@ class LabelTest extends TestCase
         $user = factory(User::class)->create();
         $token = $user->createToken('test-token');
         $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token->plainTextToken])->
-            postJson($this->api_prefix . 'add', ['name' => 1]);
+            postJson($this->api_add, ['name' => 1]);
         $response->assertStatus(HttpStatus::BadRequest)->assertJsonFragment(['name' => ["The name must be a string."]]);
         $this->assertDatabaseMissing('labels', ['name' => '1']);
     }
@@ -39,8 +41,19 @@ class LabelTest extends TestCase
     {
         $label = factory(Label::class)->make();
         $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . hash('sha256', 'fake token')])->
-            postJson($this->api_prefix . 'add', ['name' => $label->name]);
+            postJson($this->api_add, ['name' => $label->name]);
         $response->assertUnauthorized();
         $this->assertDatabaseMissing('labels', ['name' => $label->name]);
+    }
+
+    public function testCreateExistedLabelByAuthenticatedUser()
+    {
+        $user = factory(User::class)->create();
+        $token = $user->createToken('test-token');
+        $label = factory(Label::class)->create();
+        $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token->plainTextToken])->
+            postJson($this->api_add, ['name' => $label->name]);
+        $response->assertStatus(HttpStatus::BadRequest)->assertJsonFragment(['name' => ["The name has already been taken."]]);
+        $this->assertTrue(Label::where('name', $label->name)->count() == 1);
     }
 }
