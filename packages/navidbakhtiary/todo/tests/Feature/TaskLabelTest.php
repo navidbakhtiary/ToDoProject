fabcd40f1c0f1c88b2a07e739f8e5cb8ff8b1bf5
Navid Bakhtiary <navidbakhtiary@yahoo.com>
2022-05-16 10:38:31 +0430

    test: function added for the test of failure at adding label to other users task(TDD approach)

--- packages/navidbakhtiary/todo/tests/Feature/TaskLabelTest.php
+++ packages/navidbakhtiary/todo/tests/Feature/TaskLabelTest.php
@@ -28,4 +28,19 @@ class TaskLabelTest extends TestCase
             assertJsonStructure(['data' => ['task label' => ['task' => ['user' => ['id', 'name'], 'title', 'description', 'status'], 'new label' => ['id', 'name']]]]);
         $this->assertDatabaseHas('task_label', ['task_id' => $task->id, 'label_id' => $label->id]);
     }
+
+    public function testAuthenticatedUserCanNotAddLabelToOtherUserTask()
+    {
+        $user_1 = factory(User::class)->create();
+        $user_2 = factory(User::class)->create();
+        $token_1 = $user_1->createToken('test-token');
+        $task_2 = $user_2->tasks()->create(factory(Task::class)->make()->toArray());
+        $label = factory(Label::class)->create();
+        $existed_records_count = $task_2->labels()->count();
+        $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token_1->plainTextToken])->
+            postJson($this->api_add, ['task_id' => $task_2->id, 'label_id' => $label->id]);
+        $response->assertStatus(HttpStatus::BadRequest)->
+            assertJsonFragment(['errors' => ['task_id' => ['The selected task id is invalid.']]]);
+        $this->assertTrue($existed_records_count == $task_2->labels()->count());
+    }
 }
